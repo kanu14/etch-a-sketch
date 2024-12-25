d448421a88e183a0977fc88f9c6ad00d5e5ffaa4
kanu14 <mparkeh14@gmail.com>
2024-12-24 22:41:14 -0800

    added code that creates a grid

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # etch-a-sketch
+
+The Odin Project's etch-a-sketch project using html, CSS and JS.
+
+Create 16 x 16 grid of divs using js.
+Use flexbox for the grid to appear as a grid.
+Set up hover effect so the grid divs change when hovering over.
---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+// add 16 x 16 grid of divs by js
+// 16 spans with 16 divs
+
+let grid_size = 16;
+
+const container = document.getElementById('container');
+
+function createGrid() {
+    for (let i = 0; i < grid_size; i++) {
+        const span = document.createElement('span');
+        span.id = `row ${i + 1}`;
+        // span.textContent = `row ${i + 1}`;
+        container.appendChild(span);
+        for (let k = 0; k < grid_size; k++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.id = `row ${i + 1} col ${k + 1}`;
+            // gridDiv.textContent = `row ${i + 1} col ${k + 1}`;
+            
+            span.appendChild(gridDiv);
+            
+        }
+    }
+}
+
+createGrid();
